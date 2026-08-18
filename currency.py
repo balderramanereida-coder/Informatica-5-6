d9fa69a619e9b48e329d00e8bca3795a3aa8885e
balderramanereida-coder <balderramanereida@gmail.com>
2026-08-18 20:20:29 +0000

    pesos

---
+++ currency.py
@@ -0,0 +1,12 @@
+def main():
+    S=input("What do you have left in soles? ")
+    P=input("What do you have left in pesos? ")
+    R=input("What do you have left in reais? ")
+
+    mx=(S*0.0054)+(P*5.07)+(R*3.27))
+
+
+
+
+if __name__=="__main__":
+    main()
