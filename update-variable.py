4a07b83f2f2bf028b066c3b9ca7ab9bae440735c
balderramanereida-coder <balderramanereida@gmail.com>
2026-08-19 18:01:09 +0000

    Shortcuts

---
+++ update-variable.py
@@ -0,0 +1,36 @@
+def main():
+
+    # Argumented Assignment Operator
+    score= 10
+    # score= score+10
+    score += 10
+    print(score)
+
+    # Subtraction Assignment Operator
+    #x=x-y = x -= y
+    score = 100
+    score -= 15
+    print(score)
+
+    # Multiplication Assignment Operator
+    #x=x*y = x*= y
+    score = 10
+    score *= 4
+    print(score)
+
+    # Division Assignment Operator
+    x = 10
+    x /= 5
+    print(x+10)
+    # print(int(x+67))
+
+    # Modulus Assignment Operator
+    a = 10
+    b = 4
+
+    a %= b
+    print(a)
+
+
+if __name__=="__main__":
+    main()
