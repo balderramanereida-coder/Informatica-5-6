883866f8cbacfc03136c01e53583f3599c3d65ac
balderramanereida-coder <balderramanereida@gmail.com>
2026-09-04 15:48:25 +0000

    finish

---
+++ learning-app.py
@@ -0,0 +1,19 @@
+import random
+
+def main():
+    print("Welcome to my learning app")
+
+    n1 = random.randit(10, 99)
+    n2 = random.randit(10, 99)
+
+
+    while ans 
+    print(f"Try to do this:{n1}+{n2}")
+
+    ans = int(input("Type your answer: "))
+
+
+
+
+if __name__ == "__main__":
+    main()
