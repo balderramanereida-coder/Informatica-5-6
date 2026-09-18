d783a66fe190f7cd5938c2f1fa4ff5a6ae335e8e
balderramanereida-coder <balderramanereida@gmail.com>
2026-09-18 20:20:46 +0000

    b

---
+++ for-intro.py
@@ -0,0 +1,12 @@
+def main():
+
+    doctrine = ["Faith in Jesus Christ","Repentance", "Baptism by nwater","The gift of the Holy Ghost","Enduring to the end"]
+    number = ["1","2","3"]
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
