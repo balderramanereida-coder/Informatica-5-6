ccf8914f1697f99e146064dae07abdae66065481
balderramanereida-coder <balderramanereida@gmail.com>
2026-09-18 21:55:48 +0000

    weekly checkpoint 5

---
+++ Weekly checkpoint 5/To-do-list.py
@@ -0,0 +1,26 @@
+def main():
+
+    tasks = []
+
+    while True:
+        print(f"Tasks to do: {len(tasks)}")
+        print(tasks)
+        command = input("What do you want to do? (add,complete,exit): ")
+        if command == "add":
+            new_task = input("Enter new task: ")
+            tasks.append(new_task)
+
+        elif command == "complete":
+            outtask = input("Which task are you removing?: ")
+            tasks.remove(outtask)
+
+        elif command == "correct":
+            rewrite = input("What task are you rewriting?: ")
+            command[rewrite] = command
+
+        elif command == "stop":
+            break
+
+
+if __name__ == "__main__":
+    main()
--- Weekly checkpoint 5/radio-playlist.py
+++ Weekly checkpoint 5/radio-playlist.py
@@ -26,9 +26,5 @@ def main():
         time.sleep(3)
 
 
-
-
-
-
 if __name__ == "__main__":
     main()
